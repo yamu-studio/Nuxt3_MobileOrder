fe5d868e60311f4f0610a1c212a9839cba7f5efb
Yamu_Studio <95989535+yamu-studio@users.noreply.github.com>
2024-01-14 17:02:49 +0900

    -add pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,16 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ["assets/scss/index.scss", "@fortawesome/fontawesome-free/css/all.css"],
-  // modules: [
-  // '@nuxt/test-utils/module'
-  //   [
-  //     "@pinia/nuxt",
-  //     {
-  //       autoImports: ["defineStore", "acceptHMRUpdate"],
-  //     },
-  //   ],
-  // ],
-  // imports: {
-  //   dirs: ["stores"],
-  // },
+  modules: [
+    // '@nuxt/test-utils/module'
+    [
+      "@pinia/nuxt",
+      {
+        autoImports: ["defineStore", "acceptHMRUpdate"],
+      },
+    ],
+  ],
+  imports: {
+    dirs: ["stores"],
+  },
 });
